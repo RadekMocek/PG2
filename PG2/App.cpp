3c78033f9ae338706880dd1c71ab053ce41e6d1d
RadekMocek <radek.mocek@tul.cz>
2024-02-23 00:41:31 +0100

    cv01 tasks

--- PG2/App.cpp
+++ PG2/App.cpp
@@ -1,11 +1,10 @@
-// include anywhere, in any order
+// Basic includes
 #include <iostream>
 #include <chrono>
 #include <stack>
 #include <random>
 
-// OpenCV 
-// does not depend on GL
+// OpenCV – GL independent
 //#include <opencv2\opencv.hpp>
 
 // OpenGL Extension Wrangler: allow all multiplatform GL functions
@@ -22,6 +21,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// Our app
 #include "App.hpp"
 
 App::App()
@@ -31,25 +31,42 @@ App::App()
     std::cout << "Constructed...\n";
 }
 
+// App initialization, if returns true then run run()
 bool App::init()
 {
     try {
-        // init glfw
-        // https://www.glfw.org/documentation.html
-        glfwInit();
+        // Set GLFW error callback
+        glfwSetErrorCallback(error_callback);
 
-        // open window (GL canvas) with no special properties
-        // https://www.glfw.org/docs/latest/quick.html#quick_create_window
-        window = glfwCreateWindow(800, 600, "OpenGL context", NULL, NULL);
+        // Init GLFW :: https://www.glfw.org/documentation.html
+        if (!glfwInit()) {
+            return false;
+        }
+
+        // Set OpenGL version
+        /*
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        /**/
+
+        // Open window (GL canvas) with no special properties :: https://www.glfw.org/docs/latest/quick.html#quick_create_window
+        window = glfwCreateWindow(800, 600, "Moje krasne okno", NULL, NULL);
+        if (!window) {
+            glfwTerminate();
+            return false;
+        }
         glfwMakeContextCurrent(window);
+        glfwSetKeyCallback(window, key_callback);
 
-        // init glew
-        // http://glew.sourceforge.net/basic.html
-        glewInit();
+        // Init GLEW :: http://glew.sourceforge.net/basic.html
+        GLenum err = glewInit();
+        if (GLEW_OK != err) {
+            fprintf(stderr, "Error: %s\n", glewGetErrorString(err)); /* Problem: glewInit failed, something is seriously wrong. */
+        }
         wglewInit();
     }
     catch (std::exception const& e) {
-        std::cerr << "Init failed : " << e.what() << std::endl;
+        std::cerr << "Init failed : " << e.what() << "\n";
         throw;
     }
     std::cout << "Initialized...\n";
@@ -59,13 +76,13 @@ bool App::init()
 int App::run(void)
 {
     try {
+        double fpsSecondsCounter = 0;
+        int fpsFramesCounter = 0;
+
         while (!glfwWindowShouldClose(window))
         {
-            // ... do_something();
-            // 
-            // if (condition)
-            //   glfwSetWindowShouldClose(window, GLFW_TRUE);
-            // 
+            // Time/FPS measure start
+            auto fpsStart = std::chrono::steady_clock::now();
 
             // Clear OpenGL canvas, both color buffer and Z-buffer
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -75,6 +92,17 @@ int App::run(void)
 
             // Poll for and process events
             glfwPollEvents();
+
+            // Time/FPS measure end
+            auto fpsEnd = std::chrono::steady_clock::now();
+            std::chrono::duration<double> elapsed_seconds = fpsEnd - fpsStart;
+            fpsSecondsCounter += elapsed_seconds.count();
+            fpsFramesCounter++;
+            if (fpsSecondsCounter >= 1) {
+                std::cout << fpsFramesCounter << " FPS" << std::endl;
+                fpsSecondsCounter = 0;
+                fpsFramesCounter = 0;
+            }
         }
     }
     catch (std::exception const& e) {
@@ -82,6 +110,7 @@ int App::run(void)
         return EXIT_FAILURE;
     }
 
+    getInformation();
     std::cout << "Finished OK...\n";
     return EXIT_SUCCESS;
 }
@@ -89,10 +118,46 @@ int App::run(void)
 App::~App()
 {
     // clean-up
-    if (window)
+    if (window) {
         glfwDestroyWindow(window);
+    }
     glfwTerminate();
 
     exit(EXIT_SUCCESS);
     std::cout << "Bye...\n";
+}
+
+void App::getInformation() {
+    std::cout << "\n========== :: GL Info :: ==========\n";
+    std::cout << "GL Vendor:\t" << glGetString(GL_VENDOR) << "\n";
+    std::cout << "GL Renderer:\t" << glGetString(GL_RENDERER) << "\n";
+    std::cout << "GL Version:\t" << glGetString(GL_VERSION) << "\n";
+    std::cout << "GL Shading ver:\t" << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n\n";
+    
+    GLint profile;
+    glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &profile);
+
+    if (const auto errorCode = glGetError()) {
+        std::cout << "Pending GL error while obtaining profile: " << errorCode << std::endl;
+        return;
+    }
+
+    if (profile & GL_CONTEXT_CORE_PROFILE_BIT) {
+        std::cout << "Core profile" << std::endl;
+    }
+    else {
+        std::cout << "Compatibility profile" << std::endl;
+    }
+    std::cout << "===================================\n\n";
+}
+
+void App::error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void App::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
--- PG2/App.hpp
+++ PG2/App.hpp
@@ -8,4 +8,9 @@ public:
     ~App();
 private:
     GLFWwindow* window = nullptr;
+
+    void getInformation();
+
+    static void error_callback(int error, const char* description);
+    static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 };
