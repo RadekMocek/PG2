2794e55ea41f7218f04d8123d630dcd54ebaee8d
RadekMocek <radek.mocek@tul.cz>
2024-05-04 10:51:30 +0200

    AppHeightmap polish

--- PG2/AppHeightmap.cpp
+++ PG2/AppHeightmap.cpp
@@ -6,28 +6,33 @@
 
 float App::GetHeightmapY(float position_x, float position_z) const
 {    
-    float hm_x_f = position_x + HEIGHTMAP_SHIFT;
-    float hm_z_f = position_z + HEIGHTMAP_SHIFT;
-    float hm_y_f = 0.0f;
-    float hm_x_i = std::floor(hm_x_f);
-    float hm_z_i = std::floor(hm_z_f);
-    if (hm_x_f - hm_x_i < 0.5f && hm_z_f - hm_z_i < 0.5f) {
+    float X = position_x + HEIGHTMAP_SHIFT;
+    float Z = position_z + HEIGHTMAP_SHIFT;
+    float Y = 0.0f;
+
+    float X_floor = std::floor(X);
+    float Z_floor = std::floor(Z);
+
+    float X_ceil = std::ceil(X);
+    float Z_ceil = std::ceil(Z);
+
+    if (X - X_floor < 0.5f && Z - Z_floor < 0.5f) {
         // In the lower-left triangle
-        float x_fraction = hm_x_f - hm_x_i;
-        float y_fraction = hm_z_f - hm_z_i;
-        float common_height = (*_heights)[{hm_x_i, hm_z_i}];
-        float x_difference = (*_heights)[{hm_x_i + 1, hm_z_i}] - common_height;
-        float y_difference = (*_heights)[{hm_x_i, hm_z_i + 1}] - common_height;
-        hm_y_f = common_height + x_fraction * x_difference + y_fraction * y_difference;
+        float x_fraction = X - X_floor;
+        float y_fraction = Z - Z_floor;
+        float common_height = (*_heights)[{X_floor, Z_floor}];
+        float x_difference = (*_heights)[{X_ceil, Z_floor}] - common_height;
+        float y_difference = (*_heights)[{X_floor, Z_ceil}] - common_height;
+        Y = common_height + x_fraction * x_difference + y_fraction * y_difference;
     }
     else {
         // In the upper-right triangle
-        float x_fraction = hm_x_i + 1 - hm_x_f;
-        float y_fraction = hm_z_i + 1 - hm_z_f;
-        float common_height = (*_heights)[{hm_x_i + 1, hm_z_i + 1}];
-        float x_difference = common_height - (*_heights)[{hm_x_i, hm_z_i + 1}];
-        float y_difference = common_height - (*_heights)[{hm_x_i + 1, hm_z_i}];
-        hm_y_f = common_height - x_fraction * x_difference - y_fraction * y_difference;
+        float x_fraction = X_ceil - X;
+        float y_fraction = Z_ceil - Z;
+        float common_height = (*_heights)[{X_ceil, Z_ceil}];
+        float x_difference = common_height - (*_heights)[{X_floor, Z_ceil}];
+        float y_difference = common_height - (*_heights)[{X_ceil, Z_floor}];
+        Y = common_height - x_fraction * x_difference - y_fraction * y_difference;
     }
-    return hm_y_f * HEGHTMAP_SCALE;
+    return Y * HEGHTMAP_SCALE;
 }
